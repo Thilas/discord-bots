a9992da4c2adc72b6a163ed43c3e0d5798ddb593
Thomas Démoulins <tdemoulins@gmail.com>
2019-08-03 21:12:48 +0200

    Support docker upgrade

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 mode=-d
-for arg in "$*"
-do
-  case "$arg" in
-  '--test')
+while (( "$#" )); do
+  case "$1" in
+  --image-tag)
+    tag=":$2"
+    shift
+    ;;
+  --interactive)
+    mode=-it
+    ;;
+  --test)
     mode='-it --env NODE_ENV=development --rm'
     prefix=test-
     ;;
-  '--interactive'|'-i')
-    mode=-it
-    ;;
-  '')
-    ;;
   *)
-    echo "Unknown argument: $arg" 1>&2
+    echo "Unknown argument: $1" 1>&2
     exit 1
   esac
+  shift
 done
 volume="--volume $(pwd)/src/config:/home/node/app/dist/config"
-sudo docker run $mode --name ${prefix}discord-bots $volume discord/bots
+sudo docker stop ${prefix}discord-bots 2> /dev/null
+sudo docker rm ${prefix}discord-bots 2> /dev/null
+sudo docker run $mode --name ${prefix}discord-bots $volume discord/bots$tag
