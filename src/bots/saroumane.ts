a05d78556e568de1bfb444a8315131a67ee8b03c
Thomas Démoulins <tdemoulins@gmail.com>
2019-08-16 13:00:51 +0200

    Add log in watch + other improvements

--- src/bots/saroumane.ts
+++ src/bots/saroumane.ts
@@ -3,35 +3,36 @@ import saroumane from '../config/saroumane.json';
 import { Bot } from './bot';
 import * as Utils from '../utils';
 
-
 const name = 'saroumane';
 
 export class Saroumane extends Bot {
-  private answers = watch('saroumane.json', saroumane, data => {
+  private answers = watch('saroumane.json', saroumane, json => {
+    console.log(`[${name}] Available answers: yes=${json.answersYes.length}, no=${json.answersNo.length}, other=${json.answersDontKnow.length}, players=${json.answersPlayers.length}`);
     return {
-      answersYes: data.answersYes,
-      answersNo: data.answersNo,
-      answersOther: data.answersDontKnow,
-      answersPlayers: data.answersPlayers
-
-    }
+      yes: json.answersYes,
+      no: json.answersNo,
+      other: json.answersDontKnow,
+      players: json.answersPlayers
+    };
+  }, data => {
+    this.answers = data;
   });
 
   constructor(config: { token: string, triggerTellMe: string, triggerWho: string }) {
     super(name, config.token, client => {
-      client.on('message', message => {
+      config.triggerTellMe = config.triggerTellMe.toUpperCase();
+      config.triggerWho = config.triggerWho.toUpperCase();
 
-        let msg = message.content.toLowerCase();
-        let answer = "";
+      client.on('message', message => {
+        let msg = message.content.toUpperCase();
 
         if (msg.substring(0, config.triggerTellMe.length) === config.triggerTellMe) {
-          answer = this.getAnswerTellMe();
+          let answer = this.getAnswerTellMe();
           console.log(`[${name}] Reply "${answer}" to "${message.content}" from ${message.author.tag}`);
           message.reply(answer);
         }
-
         else if (msg.substring(0, config.triggerWho.length) === config.triggerWho) {
-          answer = this.getAnswerWho();
+          let answer = this.getAnswerWho();
           console.log(`[${name}] Reply "${answer}" to "${message.content}" from ${message.author.tag}`);
           message.reply(answer);
         }
@@ -40,23 +41,18 @@ export class Saroumane extends Bot {
   }
 
   private getAnswerTellMe() {
-
     let answerType = Utils.roll(3);
-
-    if (answerType == 1) {
-      return this.answers.answersYes[Utils.roll(this.answers.answersYes.length) - 1];
-    }
-
-    else if (answerType == 2) {
-      return this.answers.answersNo[Utils.roll(this.answers.answersNo.length) - 1];
-    }
-
-    else {
-      return this.answers.answersOther[Utils.roll(this.answers.answersOther.length) - 1];
+    switch (answerType) {
+      case 1:
+        return this.answers.yes[Utils.roll(this.answers.yes.length) - 1];
+      case 2:
+        return this.answers.no[Utils.roll(this.answers.no.length) - 1];
+      default:
+        return this.answers.other[Utils.roll(this.answers.other.length) - 1];
     }
   }
 
   private getAnswerWho() {
-    return this.answers.answersPlayers[Utils.roll(this.answers.answersPlayers.length) - 1];
+    return this.answers.players[Utils.roll(this.answers.players.length) - 1];
   }
 }
