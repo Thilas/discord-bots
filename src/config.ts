206c77a7af28816914cc067ca3bafc365a1060fa
Thomas Démoulins <tdemoulins@gmail.com>
2019-08-16 13:00:33 +0200

    Fix watch issue (new value never assigned)

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ const production = process.env.NODE_ENV === 'production';
 
 export const app = production ? appProduction : appDevelopment;
 
-export function watch<TIn, TOut>(filename: string, data: TIn, callback: (data: TIn) => TOut) {
+export function watch<TIn, TOut>(filename: string, data: TIn, callback: (json: TIn) => TOut, assign: (data: TOut) => void) {
   let file = path.resolve(__dirname, 'config', filename);
   fs.watch(file, event => {
     if (event === 'change') {
@@ -17,7 +17,21 @@ export function watch<TIn, TOut>(filename: string, data: TIn, callback: (data: T
         }
         if (data.length) {
           console.log(`Refreshing ${filename}...`);
-          callback(JSON.parse(data.toString()));
+          try {
+            let json = JSON.parse(data.toString());
+            try {
+              let out = callback(json);
+              try {
+                assign(out);
+              } catch (error) {
+                console.error(`Error while assigning ${filename}: ${error}`);
+              }
+            } catch (error) {
+              console.error(`Error while loading ${filename}: ${error}`);
+            }
+          } catch (error) {
+            console.error(`Error while parsing ${filename}: ${error}`);
+          }
         }
       });
     }
